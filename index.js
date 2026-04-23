bdef4a6550a0a53354737f64d3e8701c06369504
l0067388-cloud <l0067388@gmail.com>
2026-04-23 13:14:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.post("/send", async (req, res) => {
   res.send("ok");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor listo");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
+});
 });
